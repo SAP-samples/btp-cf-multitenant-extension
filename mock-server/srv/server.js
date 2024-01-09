9cbdb4ee3373ada4d1ab33a256ff5a491550630d
gopalanand333 <gopalanand333@gmail.com>
2024-01-09 14:42:31 +0530

    version update

--- mock-server/srv/server.js
+++ mock-server/srv/server.js
@@ -1,4 +1,4 @@
 const cds = require("@sap/cds");
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const proxy = require("@cap-js-community/odata-v2-adapter");
 cds.on("bootstrap", (app) => app.use(proxy()));
 module.exports = cds.server;
